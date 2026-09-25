ec2a34695b0e1bc957bca840deb5164b478556b0
blockenheimer <felipekunzccaa@gmail.com>
2026-09-25 11:35:54 -0300

    Delete bot directory

--- bot/deploy-commands.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const { syncCommands } = require('./utils/syncCommands');
-
-syncCommands().catch((error) => {
-  console.error('❌ Falha ao sincronizar comandos:', error);
-  process.exit(1);
-});
